701925df05e64346dedc83ded6fad5250d5d7534
EilonV <73593619+EilonV@users.noreply.github.com>
2023-12-12 16:09:34 +0200

    mobile/tablet view, turn off canvas option

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,9 +4,18 @@ import linkedin from '../assets/pics/icons/linkedin.svg'
 import github from '../assets/pics/icons/github.svg'
 
 export const Footer = () => {
+    let html = document.querySelector('html')
+    html.classList.add('active')
+    const changeCanvas = (e) => {
+        let isChecked = e.target.checked
+        if (isChecked)
+            html.classList.add('active')
+        else
+            html.classList.remove('active')
 
+    }
     return <section className="footer main-layout">
-        <nav>
+        <nav className='flex'>
             <ul className="flex space-between align-center">
                 <li>
                     <a href="https://www.facebook.com/dontmindmejustcleaninguphere/" target='noopener'>
@@ -27,6 +36,27 @@ export const Footer = () => {
                     <a href="https://github.com/EilonV" target='noopener'><img src={github} alt="" /></a>
                 </li>
             </ul>
+            <div className="toggle-wrapper">
+                <input className="toggle-checkbox" type="checkbox" defaultChecked onChange={changeCanvas} />
+                <div className="toggle-container">
+                    <div className="toggle-button">
+                        <div className="toggle-button-circles-container">
+                            <div className="toggle-button-circle"></div>
+                            <div className="toggle-button-circle"></div>
+                            <div className="toggle-button-circle"></div>
+                            <div className="toggle-button-circle"></div>
+                            <div className="toggle-button-circle"></div>
+                            <div className="toggle-button-circle"></div>
+                            <div className="toggle-button-circle"></div>
+                            <div className="toggle-button-circle"></div>
+                            <div className="toggle-button-circle"></div>
+                            <div className="toggle-button-circle"></div>
+                            <div className="toggle-button-circle"></div>
+                            <div className="toggle-button-circle"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </nav>
     </section>
 }
